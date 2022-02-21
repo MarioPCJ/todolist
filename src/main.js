b1f7ac5fb298f9c5694aa7781cda53f4de5ac399
MarioPCJ <mjr-12@hotmail.com>
2022-02-20 21:29:27 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -18,3 +18,6 @@ new Vue({
   render: h => h(App)
 }).$mount('#app')
 
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
